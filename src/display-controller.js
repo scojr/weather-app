5e82e901926e753fe6cf7a95b33ced9df80a3ac6
scojr <180873402+scojr@users.noreply.github.com>
2025-03-18 18:13:31 -0500

    Add display-controller module

---
+++ src/display-controller.js
@@ -0,0 +1,20 @@
+export function initiateDisplayController() {
+  const currentConditions = new CurrentConditions(document.querySelector(".primary"));
+  return { currentConditions }
+}
+
+class CurrentConditions {
+  constructor(containerElement) {
+    this.root = containerElement;
+    this.currentTemp = new TextElement(this.root.querySelector(".current-temp"))
+  }
+}
+
+class TextElement {
+  constructor(element) {
+    this.text = element;
+  }
+  setText(string) {
+    this.text.textContent = string;
+  }
+}
